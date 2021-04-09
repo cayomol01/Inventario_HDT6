e32d514af93a1c18e2107746b93390ccaddb0d60
cayomol01 <60375814+cayomol01@users.noreply.github.com>
2021-04-08 21:25:14 -0600

    Se abre el archivo y se agrega la coleccion

---
+++ Coleccion.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class Coleccion {
+    Map<String,ArrayList<String>> colection;
+
+
+    public Coleccion(Map<String,ArrayList<String>> coleccion ){
+        this.colection = coleccion;
+
+    }
+
+    
+
+
+}
--- Driver.java
+++ Driver.java
@@ -1,48 +1,64 @@
 
 import java.util.*;
+import java.io.*;
 
 class Driver{
-    public static void main(String[] args){
+    public static void main(String[] args)throws Exception{
         int contador = 0;
         Creador factory = new Creador();
         Scanner scan = new Scanner(System.in);
-        int opcionMap;
-        Map<String,String> productos;
-        LinkedList<String> prod = new LinkedList<String>();
-        prod.add("lacteos");
-        prod.add("hola que ase");
-        prod.add("lacteos");
-        HashMap<String, LinkedList<String>> hola = new HashMap<String, LinkedList<String>>();
-        TreeMap<String, String> hola1 = new TreeMap<String, String>();
+        String arch = new File("ListadoProducto.txt").getAbsolutePath();
+        File archivo = new File(arch);
+        Scanner scan_archivo = null;
+        Boolean fileOpened;
+        String[] text_archivo;
+        HashMap<String, ArrayList<String>> inventario = new HashMap<String, ArrayList<String>>();
+        Map<String,ArrayList<String>> coleccion;
+        String key; 
 
-        hola.put("primero", prod);
-        System.out.println(hola1);
-        hola.put("segundo", new LinkedList<String>());
-        hola.put("a", new LinkedList<String>());
-        System.out.println(hola);
-        for(int i = 0; i<hola.get("primero").size(); i++){
-            System.out.println(hola);
-            if(hola.get("primero").get(i).equals("lacteos")){
-                contador++;
-            }
+
+
+
+        try {
+            scan_archivo = new Scanner(archivo);
+            fileOpened = true;
+            System.out.println("Se ha leído su archivo de inventario");
         }
-        System.out.println(hola.hashCode());
-        hola.get("segundo");
-        System.out.println(hola);
+        catch (FileNotFoundException e) {
+            System.out.println("--- File Not Found! ---");
+            fileOpened = false;
+        }
+        scan_archivo.useDelimiter("\\Z");
+        int opcionMap;
+ 
+        if(fileOpened){
+            while(scan_archivo.hasNextLine()){
+                text_archivo = scan_archivo.nextLine().split("\t");
+                key = text_archivo[0].substring(0,text_archivo[0].length()-1);
+                if(!inventario.containsKey(key)){
+                    inventario.put(key, new ArrayList<String>());
+                }
+                inventario.get(key).add(text_archivo[1]);
+                System.out.println(key + " " + inventario.get(key).toString());
 
-        
-        System.out.println(contador);
+            }
 
-        System.out.println("Bienvenido al inventario digital!\n");
-        System.out.println("Para empezar, quisiera preguntar ¿Que tipo de estructura desea utilizar?");
-        System.out.println("1. HashMap");
-        System.out.println("2. TreeMap");
-        System.out.println("3. LinkedHashMap");
 
-        System.out.println("Por favor escriba el número de la opción que desea utlizar");
-        opcionMap = scan.nextInt();
-        productos = factory.Crear(opcionMap); 
+            System.out.println("Bienvenido al inventario digital!\n");
+            System.out.println("Para empezar, quisiera preguntar ¿Que tipo de estructura desea utilizar?");
+            System.out.println("1. HashMap");
+            System.out.println("2. TreeMap");
+            System.out.println("3. LinkedHashMap");
 
-        System.out.println("\nQué desea realizar con su inventario?");
+            System.out.println("Por favor escriba el número de la opción que desea utlizar");
+            opcionMap = scan.nextInt();
+            coleccion = factory.Crear(opcionMap); 
+
+            System.out.println("\nQué desea realizar con su inventario?");
+            System.out.println("1. Agregar un nuevo producto a su coleccion");
+        }
+        else{
+            System.out.println("No se pudo empezar el programa debido a que no se encontró el archivo");
+        }
     }
 }
