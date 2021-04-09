d24f77d899ec071a821d5b6f8252af92a308b2ff
cayomol01 <60375814+cayomol01@users.noreply.github.com>
2021-04-08 23:40:58 -0600

    Se hizo la primera opcion

--- Coleccion.java
+++ Coleccion.java
@@ -2,15 +2,89 @@
 import java.util.*;
 
 public class Coleccion {
-    Map<String,ArrayList<String>> colection;
+    HashMap<String,ArrayList<String>> inventario;
+    Map<String,ArrayList<String>> coleccion;
+    HashMap<String, Integer> cant;
+    ArrayList<String> keys;
+    Boolean contains = false;
+    String key;
+    int contador = 0;
+    int contador2 = 0;
 
 
-    public Coleccion(Map<String,ArrayList<String>> coleccion ){
-        this.colection = coleccion;
+    public Coleccion(HashMap<String,ArrayList<String>> inventario, Map<String,ArrayList<String>> coleccion){
+        this.inventario = inventario;
+        this.coleccion = coleccion;
 
     }
 
+    public void setKeys(ArrayList<String> keys){
+        this.keys = keys;
+    }
+
+    private void Cant(){
+        for(int i = 0; i<keys.size(); i++){
+         
+        }
+    }
+
+    public Boolean Contains(String value){
+        contains = false;
+        contador = 0;
+        while(!contains && contador < keys.size()){
+            contador2 = 0;
+            while(contador2<inventario.get(keys.get(contador)).size() && !contains){
+                if(inventario.get(keys.get(contador)).get(contador2).equals(value)){
+                    contains = true;
+                    key = keys.get(contador);
+                    break;
+                }
+                else{
+                    contains = false;
+                }
+                contador2++;
+            }
+            contador++;
+        }
+        return contains;
+
+        
+    }
+
+    public void Agregar(String producto){
+        if(Contains(producto)){
+            if(!coleccion.containsKey(key)){
+                coleccion.put(key, new ArrayList<String>());
+                
+                
+            }
+            coleccion.get(key).add(producto);
+            System.out.println("Su producto se ha agregado efectivamente...\n");
+        }
+        else{
+            System.out.println("\nNO se pudo encontrar el producto deseado en nuestro inventario...\n");
+        }
+    }
+
     
+    public void Mostrar(int opcion, String producto){
+
+    }
+
+    public void Categoria(String producto){
+        for(int i = 0; i< keys.size(); i++){
+            for(int j = 0; j<inventario.get(keys.get(i)).size(); j++){
+                if(producto.equals(inventario.get(keys.get(i)).get(j))){
+                    System.out.println("Producto: " + producto + " Categoría: " + keys.get(i));
+                }
+                else{
+                    System.out.println("No se pudo encontrar el producto en nuestro inventario");
+                }
+            }
+        }
+    }
+
+
 
 
 }
--- Driver.java
+++ Driver.java
@@ -5,6 +5,7 @@ import java.io.*;
 class Driver{
     public static void main(String[] args)throws Exception{
         int contador = 0;
+        int opcionInventario = 0;
         Creador factory = new Creador();
         Scanner scan = new Scanner(System.in);
         String arch = new File("ListadoProducto.txt").getAbsolutePath();
@@ -14,8 +15,11 @@ class Driver{
         String[] text_archivo;
         HashMap<String, ArrayList<String>> inventario = new HashMap<String, ArrayList<String>>();
         Map<String,ArrayList<String>> coleccion;
-        String key; 
-
+        String key;
+        ArrayList<String> Keys = new ArrayList<String>();
+        Coleccion col = null;
+        String producto = "";
+        String temp;
 
 
 
@@ -34,31 +38,53 @@ class Driver{
         if(fileOpened){
             while(scan_archivo.hasNextLine()){
                 text_archivo = scan_archivo.nextLine().split("\t");
-                key = text_archivo[0].substring(0,text_archivo[0].length()-1);
+                key = text_archivo[0].substring(0,text_archivo[0].length()-2);
+                Keys.add(key);
                 if(!inventario.containsKey(key)){
                     inventario.put(key, new ArrayList<String>());
                 }
                 inventario.get(key).add(text_archivo[1]);
-                System.out.println(key + " " + inventario.get(key).toString());
 
             }
+            
 
 
-            System.out.println("Bienvenido al inventario digital!\n");
+            System.out.println("\nBienvenido al inventario digital!");
             System.out.println("Para empezar, quisiera preguntar ¿Que tipo de estructura desea utilizar?");
             System.out.println("1. HashMap");
             System.out.println("2. TreeMap");
             System.out.println("3. LinkedHashMap");
-
             System.out.println("Por favor escriba el número de la opción que desea utlizar");
+
             opcionMap = scan.nextInt();
             coleccion = factory.Crear(opcionMap); 
+            col = new Coleccion(inventario, coleccion);
+            col.setKeys(Keys);
+            while(opcionInventario != 7){
+                System.out.println("\nQué desea realizar con su inventario?");
+                System.out.println("1. Agregar un nuevo producto a su coleccion");
+                System.out.println("2. Mostrar la categoría de un producto ingresado");
+                System.out.println("3. Mostrar la categoría y cantidad de un producto ingresado");
+                System.out.println("4. Mostrar la categoría, cantidad de cada producto en su coleccion");
+                System.out.println("5. Mostrar todo el inventario");
+                System.out.println("6. Mostrar su coleccion");
+                System.out.println("7. Salir");
 
-            System.out.println("\nQué desea realizar con su inventario?");
-            System.out.println("1. Agregar un nuevo producto a su coleccion");
+                opcionInventario = scan.nextInt();
+                scan.nextLine();
+
+                if(opcionInventario == 1){
+                    System.out.println("hola");
+                    producto = scan.nextLine();
+                    System.out.println(producto);
+                    col.Agregar(producto);
+                }
+            }
+
         }
         else{
             System.out.println("No se pudo empezar el programa debido a que no se encontró el archivo");
         }
+
     }
 }
